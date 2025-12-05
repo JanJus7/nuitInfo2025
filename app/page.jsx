f3d28bb5a857ca4f4884b86822e1826e6cc8a554
Jan <janjus2004@gmail.com>
2025-12-05 07:00:14 +0100

    Added chatbot function

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
 import HomeContent from "./components/HomeContent";
 
 export default function Home() {
-  return <HomeContent />;
+  return (
+    <main>
+      <HomeContent />
+      <ChatWidget />
+    </main>
+  );
 }
